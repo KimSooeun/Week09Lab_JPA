b2abea3f256384ae496a3aa40094f73cd9f1cbed
817949 <817949@SAIT224420IT.ACDM.DS.SAIT.CA>
2020-11-22 16:52:18 -0700

    Finished Lab9

---
+++ src/java/services/UserService.java
@@ -0,0 +1,46 @@
+package services;
+
+import database.UserDB;
+import java.util.List;
+import models.User;
+
+public class UserService {
+    private UserDB userDB;
+    
+    public UserService() {
+    }
+    
+    public User get(String username) throws Exception {
+        this.userDB = new UserDB();
+        return this.userDB.getUser(username);
+    }
+
+    public List<User> getAll() throws Exception {
+        this.userDB = new UserDB();
+        return this.userDB.getAll();
+    }
+
+    public void update(String username, String password, String firstname, String lastname, String email) throws Exception {
+        this.userDB = new UserDB();
+        User user = this.userDB.getUser(username);
+        if ( !password.equals("") ) user.setPassword(password);
+        if ( !firstname.equals("") ) user.setFirstname(firstname);
+        if ( !lastname.equals("") ) user.setLastname(lastname);
+        if ( !email.equals("") ) user.setEmail(email);
+        this.userDB.update(user);
+    }
+
+    public void delete(String username) throws Exception {
+        this.userDB = new UserDB();
+        this.userDB.delete(username);
+    }
+
+    public void insert(String username, String password, String firstname, String lastname, String email) throws Exception {
+        this.userDB = new UserDB();
+        User user = new User(username, password);
+        if ( !firstname.equals("") ) user.setFirstname(firstname);
+        if ( !lastname.equals("") ) user.setLastname(lastname);
+        if ( !email.equals("") ) user.setEmail(email);
+        this.userDB.insert(user);
+    }
+}
